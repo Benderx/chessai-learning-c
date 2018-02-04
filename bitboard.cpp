80b646920cba2b843ad493d0fded59e9434151c5
Matthew <mze5111@gmail.com>
2018-02-04 18:27:15 -0500

    Added functionality for lsb lookup table

--- bitboard.cpp
+++ bitboard.cpp
@@ -1,5 +1,7 @@
 #include "bitboard.hpp"
 #include <stdlib.h>
+#include <unordered_map>
+#include <cmath>
 
 
 
@@ -70,73 +72,11 @@ void Engine::init_position(unsigned long long *board_data)
 
 void Engine::init_lsb_lookup()
 {
-    std::unordered_map <unsigned long long, int> lsb_lookup;
-    lsb_lookup = 
+    std::unordered_map<unsigned long long, int> lsb_lookup;
+    
+    for(int i=0;i<63;i++)
     {
-        {,0},
-        {,1},
-        {,2},
-        {,3},
-        {,4},
-        {,5},
-        {,6},
-        {,7},
-        {,8},
-        {,9},
-        {,10},
-        {,11},
-        {,12},
-        {,13},
-        {,14},
-        {,15},
-        {,16},
-        {,17},
-        {,18},
-        {,19},
-        {,20},
-        {,21},
-        {,22},
-        {,23},
-        {,24},
-        {,25},
-        {,26},
-        {,27},
-        {,28},
-        {,29},
-        {,30},
-        {,31},
-        {,32},
-        {,33},
-        {,34},
-        {,35},
-        {,36},
-        {,37},
-        {,38},
-        {,39},
-        {,40},
-        {,41},
-        {,42},
-        {,43},
-        {,44},
-        {,45},
-        {,46},
-        {,47},
-        {,48},
-        {,49},
-        {,50},
-        {,51},
-        {,52},
-        {,53},
-        {,54},
-        {,55},
-        {,56},
-        {,57},
-        {,58},
-        {,59},
-        {,60},
-        {,61},
-        {,62},
-        {,63},
+        lsb_lookup.insert({(unsigned long long) std::pow(2,i),i});
     }
 }
 
