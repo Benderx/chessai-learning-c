a64e119004a6b272e61a525591c17ae3eec0e2b7
Matthew <mze5111@gmail.com>
2018-02-10 16:37:09 -0500

    Inital commit

---
+++ player.hpp
@@ -0,0 +1,37 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include "bitboard.hpp"
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+class Player
+{
+	public:
+		//Vars
+		int color;
+		Engine *e;
+
+		//Methods
+		Player(int col, Engine *engine);
+		int get_color();
+
+	private:
+
+};
+
+class Rand: public Player
+{
+	public:
+		//Var
+
+		//Methods
+		Rand(int col, Engine *engine)
+		unsigned long long move()
+
+	private:
+};
+
+
+#endif
