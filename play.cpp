6502c1b69088a8b9e833862a922daf0b27d1e5ea
Andrew Duerig <duerig.andrew@gmail.com>
2018-02-13 02:23:02 -0500

    Moved Rand.cpp to player.cpp

--- play.cpp
+++ play.cpp
@@ -4,18 +4,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// g++ bitboard.hpp bitboard.cpp play.cpp -std=c++14 -o run
+
 int main()
 {
 	Engine* e = new Engine();
-	Rand *white_player = new Rand(1,e);
-	Rand *black_player = new Rand(0,e);
+	Rand *white_player = new Rand(1, e);
+	Rand *black_player = new Rand(0, e);
 
 	max_moves  = e->get_max_move_length();
 	int moves_made = 0;
 
 	while(moves_made < max_moves)
 	{
-		std::cout << "White to move." << endl;
+		std::cout << "White to move." << std::endl;
 		
 	}
 }
--- player.cpp
+++ player.cpp
@@ -23,3 +23,27 @@ unsigned long long Rand::move()
 {
 	std::cout << e->generate_legal_moves(color) << std::endl;
 }
+
+class Rand
+{
+	public:
+
+		int color;
+		Engine *e;
+
+		Rand(int col, Engine *engine)
+		{
+			color = col;
+			e = engine;
+		}
+
+		int get_color()
+		{
+			return(this->color);
+		}
+
+		unsigned long long move()
+		{
+			std::cout << e->generate_legal_moves(color) << std::endl;
+		}
+};
